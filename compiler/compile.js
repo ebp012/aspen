4bd24e9b5c3abc636c61b08f0ded56f999ad7a15
ebp012 <96751029+ebp012@users.noreply.github.com>
2024-09-17 18:09:53 -0400

    Update compile.js

--- compiler/compile.js
+++ compiler/compile.js
@@ -173,16 +173,6 @@ function tetration(a, b) {
 // Get the input field
 var inputk = document.getElementById("commandBlock");
 
-// Execute a function when the user presses a key on the keyboard
-inputk.addEventListener("keypress", function(event) {
-	// If the user presses the "Enter" key on the keyboard
-	if (event.key === "Enter") {
-		// Cancel the default action, if needed
-		event.preventDefault();
-		// Trigger the button element with a click
-		document.getElementById("doBtn").click();
-	}
-});
 var isExpanded = false;
 
 function toggleExpand() {
