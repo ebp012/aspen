fa1ff99e748f40e1641dfefc95fe190afb3686e4
ebp012 <96751029+ebp012@users.noreply.github.com>
2024-09-28 13:17:53 -0400

    Add five more routines to the standard library
    Add:
    - sleep()
    - time()
    - instant()
    - color()
    - bgcolor()

--- compiler/aspen.js
+++ compiler/aspen.js
@@ -1,14 +1,45 @@
 var aspen = {
-  print: function (text) {
-    aspenConsole.innerText += "\n" + text + "\n";
-  },
-  note: function (text) {
-    aspenConsole.innerText += text;
-  },
-  warn: function (text) {
-    aspenConsole.innerHTML += "<br/><p style='color: gold;'>" + text + "</p><br/>";
-  },
-  clear: function (text) {
-    aspenConsole.innerText = "";
-  }
+	// Output Routines
+		// Print text with newlines
+		print: function (text) {
+			aspenConsole.innerText += "\n" + text + "\n";
+		},		
+		// Print text without newlines
+		note: function (text) {
+			aspenConsole.innerText += text;
+		},		
+		// Warn users
+		warn: function (text) {
+			aspenConsole.innerHTML += "<p style='color: gold;'>" + text + "</p><br/>";
+		},
+	
+	// Basic Functions
+		// Sleep for a specified amount of seconds
+		sleep: function (time) {
+			return new Promise(resolve => setTimeout(resolve, time * 1000));
+		},
+		// Get the current time and continuously append it
+		time: function () {
+			setInterval(function() {
+				aspenConsole.innerText += new Date().toLocaleTimeString() + "\n";
+			}, 1000);
+		},
+		// Get the time at the instant the routine is run
+		instant: function () {
+			aspenConsole.innerText += new Date().toLocaleTimeString() + "\n";
+		},
+	
+	// Console Routines
+		// Change console text colour
+		color: function (colour) {
+			aspenConsole.style.color = colour;
+		},
+		// Change console background colour
+		bgcolor: function (colour) {
+			aspenConsole.style.backgroundColor = colour;
+		},
+		// Clear the console
+		clear: function () {
+			aspenConsole.innerText = "";
+		}
 }
