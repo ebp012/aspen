bdaba6643e2b9e32abf2f834060a39e7d488999d
ebp012 <96751029+ebp012@users.noreply.github.com>
2024-10-01 20:10:20 -0400

    Update upload.js

--- compiler/upload.js
+++ compiler/upload.js
@@ -1,2 +1,18 @@
 const input = document.querySelector("input[type=file]");
 
+function uploadAspen () {
+	const reader = new FileReader();
+	
+	reader.onload = function (e) {
+		uploadTxt = e.target.result; // Save contents to variable uploadTxt
+		
+		if (uploadTxt != undefined) {
+			console.log(uploadTxt); // Put debug info in console
+		}
+		else {
+			console.log("File upload failed");
+		}
+		
+		document.getElementById("commandBlock").innerHTML = uploadTxt; // Retrieve it
+	};
+}
