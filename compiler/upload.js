ccdd308c7f00012c9cfef8c13cc50ff3a2531181
ebp012 <96751029+ebp012@users.noreply.github.com>
2024-10-11 08:28:40 -0400

    File upload capabilities

--- compiler/upload.js
+++ compiler/upload.js
@@ -1,26 +1,13 @@
-const input = document.querySelector("input[type=file]");
-
-function uploadAspen() {
-	// Check if a file has been selected
-	if (input.files.length === 0) {
-		console.log("No file selected");
-		return; // Exit the function if no file is selected
+function readFile() {
+	let fileInput = document.getElementById("codeUpload");
+	let textArea = document.getElementById("commandBlock");
+	let inputFile = fileInput.files;
+	const file = inputFile[0];
+	let reader = new FileReader();
+	reader.onload = (e) => {
+		const file = e.target.result;
+		const lines = file.split(/\r\n|\n/);
+		textArea.innerText = lines.join('\n');
 	}
-	
-	const reader = new FileReader();
-	
-	reader.onload = function(e) {
-		const uploadTxt = e.target.result; // Save contents to variable uploadTxt
-		
-		if (uploadTxt !== undefined) {
-			console.log(uploadTxt); // Put debug info in console
-			document.getElementById("commandBlock").innerHTML = uploadTxt; // Retrieve it
-		}
-		else {
-			console.log("File upload failed");
-		}
-	};
-	
-	// Read the file as text
-	reader.readAsText(input.files[0]);
+	reader.readAsText(file);
 }
