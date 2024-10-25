92683e3d23caf4b4ebdf0ba7f7277024a704a5f7
ebp012 <96751029+ebp012@users.noreply.github.com>
2024-10-25 05:40:15 -0400

    Update math.js

--- compiler/libs/math.js
+++ compiler/libs/math.js
@@ -17,12 +17,7 @@ var math = {
 		// Detect or solve difference of perfect squares
 		dops: function (a, b, c, factor) {
 			if (Math.sqrt(a) % 1 == 0 && b == 0 && Math.sqrt(c) % 1 == 0) {
-				if (factor != false) {
-					return "(" + Math.sqrt(a) + "x+" + Math.sqrt(c) + ")(" + Math.sqrt(a) + "x-" + Math.sqrt(c) + ")";
-				}
-				else {
-					return true;
-				}
+				return true;
 			}
 			else {
 				return false;
@@ -31,29 +26,90 @@ var math = {
 	},
 	// Cubic Functions
 	cube: {
-		dopc: function (a, b, c, d, factor, complete) {
+		dopc: function (a, b, c, d) {
 			if (Math.cbrt(a) % 1 == 0 && b == 0 && c == 0 && Math.cbrt(d) % 1 == 0) {
-				if (factor != false) {
-					if (complete != false) {
-						var dopsCheck = math.quad.dops(Math.cbrt(a) ** 2, (Math.cbrt(a) * Math.cbrt(d)), Math.cbrt(d) ** 2, false);
-						if (dopsCheck != true) {
-							return math.cube.dopc(a, b, c, d, factor, false);
-						}
-						else {
-							return "(" + Math.cbrt(a) + "x-" + Math.cbrt(d) + ")" + math.quad.dops(Math.cbrt(a) ** 2, (Math.cbrt(a) * Math.cbrt(d)), Math.cbrt(d) ** 2, true);
-						}
-					}
-					else {
-						return "(" + Math.cbrt(a) + "x-" + Math.cbrt(d) + ")(" + Math.cbrt(a) + "x^3+" + (Math.cbrt(a) * Math.cbrt(b)) + "+" + Math.cbrt(d) + ")";
-					}
-				}
-				else {
-					return true;
-				}
+				return -1;
+			}
+			else if (Math.cbrt(a) % 1 == 0 && b == 0 && c == 0 && Math.cbrt(d) % 1 == 0) {
+				return 1;
 			}
 			else {
-				return false;
+				return 0;
 			}
 		}
+	},
+	// Complex Numbers
+	complex: {
+		plus: function (a1, b1, a2, b2, datatype) {
+			if (datatype != "string") {
+				return [2, a1 + a2, b1 + b2];
+			}
+			else {
+				if (b1 + b2 < 0) {
+					return (a1 + a2) + "-" + (-b1 - b2) + "i";
+				}
+				else {
+					return (a1 + a2) + "+" + (b1 + b2) + "i";
+				}
+			}
+		},
+		minus: function (a1, b1, a2, b2, datatype) {
+			if (datatype != "string") {
+				return [2, a1 - a2, b1 - b2];
+			}
+			else {
+				if (b1 + b2 < 0) {
+					return (a1 + a2) + "+" + (-b1 + b2) + "i";
+				}
+				else {
+					return (a1 + a2) + "-" + (b1 - b2) + "i";
+				}
+			}
+		},
+		times: function (a, b, c, d, datatype) {
+			if (datatype != "string") {
+				// (a + bi)(c + di)
+				// ac + adi + bci + bdi^2
+				// ((a*c) + (a*d) - (b*d)) + (b*c)i
+				return [2, (a*c) + (a*d) - (b*d), b*c];
+			}
+			else {
+				return ((a*c) + (a*d) - (b*d)) + (b * c) + "i";
+			}
+		},
+	},
+	trig: {
+		// Regular functions (12)
+		sin: function(theta) { return Math.sin(theta); }, // sine
+		cos: function(theta) { return Math.cos(theta); }, // cosine
+		tan: function(theta) { return Math.tan(theta); }, // tangent
+		csc: function(theta) { return 1/(Math.sin(theta)); }, // cosecant
+		sec: function(theta) { return 1/(Math.cos(theta)); }, // secant
+		cot: function(theta) { return 1/(Math.tan(theta)); }, // cotangent
+		vsn: function(theta) { return 1-(Math.sin(theta)); }, // versine
+		cvs: function(theta) { return 1-(Math.cos(theta)); }, // coversine
+		xsc: function(theta) { return (math.trig.sec(theta)) - 1; }, // exsecant
+		xcc: function(theta) { return (math.trig.csc(theta)) - 1; }, // excosecant
+		crd: function(theta) { return Math.sqrt((math.trig.sin(theta) ** 2) + (math.trig.vsn(theta) ** 2)); }, // chord
+		arc: function(theta) { return theta * 2 * Math.pi; }, // arc
+		// Hyperbolic functions (12)
+		sinh: function(theta) { return Math.sinh(theta); }, // hyperbolic sine
+		cosh: function(theta) { return Math.cosh(theta); }, // hyperbolic cosine
+		tanh: function(theta) { return Math.tanh(theta); }, // hyperbolic tangent
+		csch: function(theta) { return 1/(Math.sinh(theta)); }, // hyperbolic cosecant
+		sech: function(theta) { return 1/(Math.cosh(theta)); }, // hyperbolic secant
+		coth: function(theta) { return 1/(Math.tanh(theta)); }, // hyperbolic cotangent
+		vsnh: function(theta) { return 1-(Math.sinh(theta)); }, // hyperbolic versine
+		cvsh: function(theta) { return 1-(Math.cosh(theta)); }, // hyperbolic coversine
+		xsch: function(theta) { return (math.trig.sech(theta)) - 1; }, // hyperbolic exsecant
+		xcch: function(theta) { return (math.trig.csch(theta)) - 1; }, // hyperbolic excosecant
+		crdh: function(theta) { return 2 * Math.sinh(theta / 2); }, // hyperbolic chord
+		arch: function(theta) { return theta * 2 * Math.pi; }, // hyperbolic arc
+		// Euler's Identity
+		euler: function(theta) {
+			var a = Math.cos(theta) + math.complex.times(0, 1, Math.sin(theta), 0)[1]; // Real part of number
+			var b = Math.cos(theta) + math.complex.times(0, 1, Math.sin(theta), 0)[2]; // Imaginary part of number
+			return [2, a, b]; // Return as array that can be printed by the programmer
+		}, // e^iθ = cosθ + (i * sinθ)
 	},
 }
